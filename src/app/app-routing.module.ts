fcba2f52ea60a693ed95c99a6f6549d4f88f57b1
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2018-08-26 20:49:13 -0500

    Added routes to navigate between components. Notice (its important) don't use 'href' on '<a>' because the SPA doesn't work, instead of use 'routerLink'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const appRoutes: Routes = [];
+// TODO: add modules to every component in order to manage their own routing configuration
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'conversation', component: ConversationComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [
