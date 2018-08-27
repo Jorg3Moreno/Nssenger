1616646fadc86a0404d557159690c4f7ba5c3260
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2018-08-26 20:09:04 -0500

    Conversation Component: this component manage conversations with friends.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ConversationComponent
   ],
   imports: [
     BrowserModule
